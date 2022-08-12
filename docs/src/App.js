da44c39423ea67c4d2cc6f15e8aa9c6ce033b698
Kevin Krpicak <kkrpicak@gmail.com>
2022-08-12 11:26:16 -0400

    setup docs rendering (create react app)

---
+++ docs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import {Button} from '@kevinkrpicak/test-component-lib';
+
+function App() {
+  return (
+    <>
+      <Button label="Imported Button!" />
+    </>
+  );
+}
+
+export default App;
