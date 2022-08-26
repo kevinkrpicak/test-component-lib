f958fbf87302a8e007387a728c17e9281427b091
Kevin Krpicak <kkrpicak@gmail.com>
2022-08-25 21:44:06 -0400

    various cleanup, fix randomness

--- docs/src/App.js
+++ docs/src/App.js
@@ -7,7 +7,27 @@ function App() {
   return (
     <>
       <H1>Test Component Page</H1>
-      <Button label="Imported Button!" />
+      <Button
+        className="foo"
+        style={{ padding: "10px" }}
+        label="Imported Button!"
+      />
+      <Button
+        as="a"
+        className="bar me-2"
+        data-foo="bar"
+        label="Imported Button!"
+      />
+      <Button as="div" className="foo bar" label="Imported Button!" />
+
+      <div className="d-flex justify-content-between">
+        <Button className="foo flex-grow-1" label="Imported Button!" />
+        <Button as="a" className="flex-grow-1" label="Imported Button!" />
+        <Button
+          as="div"
+          className="foo bar flex-grow-1"
+        >Child Content</Button>
+      </div>
     </>
   );
 }
