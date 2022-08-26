a83dc64d3ccacef561918b5e8ef8c86d1eb75e43
Kevin Krpicak <kkrpicak@gmail.com>
2022-08-25 21:43:39 -0400

    updates for button component, pass attributes, children, etc

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,22 @@
 import React from "react";
 import css from './Button.css';
 
-const Button = (props) => {
-  return (
-    <button className={css.button}>{props.label}</button>
+const Button = ({as, className, label, children, ...props}) => {
+  const acceptedTypes = [
+    "div", "span", "a"
+  ];
+
+  return React.createElement(
+    acceptedTypes.includes(as) && as || 'button',
+    {
+      className: [css.button, className].join(' '),
+      ...props
+    },
+    children || label
   );
+//   (
+//     <button {...props} className={[css.button, props.className].join(' ')}>{props.children}</button>
+//   );
 }
 
 export default Button;
