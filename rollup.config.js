9dd38ee93b2c5ad680cccd1ec0f522c3f3237f63
Kevin Krpicak <kkrpicak@gmail.com>
2022-10-01 02:12:43 -0400

    use ts everywhere

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,47 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import postcss from "rollup-plugin-postcss";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.js",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
     plugins: [
       resolve(),
       babel({
-        babelHelpers: "bundled",
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-env", "@babel/preset-react"],
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-env', '@babel/preset-react'],
       }),
       commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
       postcss({
         extract: false,
         modules: true,
-        use: ["sass"],
+        use: ['sass'],
       }),
     ],
   },
   {
-    input: "dist/esm/index.js",
-    output: [{ file: "dist/index.js", format: "esm" }],
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
     external: [/\.(css|less|scss)$/],
   },
 ];
---
+++ src/components/Heading/Heading.ts
@@ -0,0 +1,39 @@
+import React from 'react';
+import css from './Heading.module.css';
+// import './Heading.css';
+
+enum HeadingTypes {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  H5 = 'h5',
+  H6 = 'h6',
+}
+
+type HeadingProps = {
+  as?: HeadingTypes;
+  className?: string;
+  label?: string;
+  children?: React.ReactNode;
+};
+
+// TODO: Use 'HeadingProps' type for destructured props
+const Heading = ({
+  as = HeadingTypes.H1,
+  className,
+  label,
+  children,
+  ...props
+}: HeadingProps) => {
+  return React.createElement(
+    as,
+    {
+      className: [css[as], className].join(' '),
+      ...props,
+    },
+    children || label
+  );
+};
+
+export default Heading;
